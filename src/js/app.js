8316c03b691124ad68ae8e11cae7e4cb8c759e01
Николай Лабазов <Nickolaylabazov@yandex.ru>
2019-05-06 21:29:08 +0300

    6.05

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
-import { stor } from './storageStandard.js';
-import { stream } from './streamStandard.js';
-import { req } from './XMLHttpRequest Standard.js';
+import { stor } from './StorageStandard.js';
+import { stream } from './StreamStandard.js';
+import { req } from './XMLHttpRequestStandard.js';
 import DownloadManager from './downloadManager.js';
 
 const content = [['Storage Standard', '304 kb', stor], ['Streams Standard', '1655 kb', stream], ['XMLHttpRequest Standard', '814 kb', req]];
